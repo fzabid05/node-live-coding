aab70d18c092e988fb6577a84ae88a5ad7b7ca4b
fzabid05 <abidzora88@gmail.com>
2020-11-13 17:30:17 +0100

    set up TypeScript, convert js files to ts

---
+++ src/controllers/wilder.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+const WilderModel = require("../models/Wilder");
+
+export default {
+  get: async (req: Request, res: Response) => {
+    const wilders = await WilderModel.find({});
+    res.json({ wilders });
+  },
+  create: async (req: Request, res: Response) => {
+    await WilderModel.init();
+    const wilder = new WilderModel(req.body);
+    const result = await wilder.save();
+    res.status(201).json({ result });
+  },
+  update: async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await WilderModel.findOneAndUpdate({ _id: id }, req.body, {
+      new: true,
+    });
+    res.json({ result });
+  },
+  delete: async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const result = await WilderModel.findByIdAndDelete(id);
+    res.json({ result });
+  },
+};
--- src/models/Wilder.ts
+++ src/models/Wilder.ts
@@ -1,9 +1,11 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
-const WilderSchema = new Schema({
+const WilderSchema = new Schema 
+({
     name: { type: String, unique: true },
   city: String,
   skills: [{ title: String, voteCount: Number }],
 });
+
 module.exports = mongoose.model("wilder", WilderSchema);
